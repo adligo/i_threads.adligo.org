f5cf70e29d54fe88baaf24167ac95510f4b8bb3c
Scott Morgan <scott@adligo.com>
2022-01-12 08:30:34 -0600

    removde a method and some javadoc cleanup

--- src/org/adligo/i/threads/I_ThreadCtx.java
+++ src/org/adligo/i/threads/I_ThreadCtx.java
@@ -2,15 +2,17 @@ package org.adligo.i.threads;
 
 /**
  * This interface contains adaptor methods for basic threading used by JSE 
- * that are also in use by JME / CLDC / MEEP / MIDP.
+ * that are also in use by JME / CLDC / MEEP / MIDP. <br/><br/>
+ * Like all of Adligp's Context (abbreviated Ctx) facilitate ths Stubbing Pattern with tools like;
+ * {@link <a href="https://github.com/adligo/mockito_ext.adligo.org">mockito_ext.adligo.org</a>}<br/><br/>
  *   However it is missing many JSE features like Executors and ThreadPools
- *  that are available in I_ThreadJseCtx in the i_threads4jse.adligo.org project.
- *    Like all of Adligo's Ctx (Context) classes this provides a way to 
- * stub out the creation of various methods using mockito_ext.adligo.org.
+ *  that are available in I_ThreadJseCtx in the i_threads4jse.adligo.org project.  
+ *  For that functionality look at;
+ *  {@link <a href="https://github.com/adligo/i_threads4jse.adligo.org">i_threads4jse.adligo.org</a>}<br/><br/>
  *  
- * @author scott
+ * @author scott<br/><br/>
  *
- * 
+ * <pre><code>
  *  ---------------- Apache ICENSE-2.0 --------------------------
  *
  * Copyright 2022 Adligo Inc
@@ -26,30 +28,17 @@ package org.adligo.i.threads;
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ * </code></pre>
  */
 public interface I_ThreadCtx {
 
-  /**
-   * This will eventually return new VirtualThreads when Project Loom's
-   * Virtual Thread are promoted from pre-release to actually released;
-   * 
-   * https://www.youtube.com/watch?v=EO9oMiL1fFo
-   * No more thread Pools ? async await is 'thread like' but not a real thread
-   * https://download.java.net/java/early_access/loom/docs/api/java.base/java/util/concurrent/Executors.html#newVirtualThreadPerTaskExecutor()
-   * Throughput optimization formula 
-   * 
-   * Brian Gotez 'Reactive Coding is dead'
-   * records are a surrogate for Tuples
-   * https://www.youtube.com/watch?v=9si7gK94gLo
-   * 
-   * @return either a good old fashioned Java Thread or a Virtual Thread
-   */
-  public Thread newThread();
 
   /**
    * This toggles the Threads interrupt, it provides the ability
    * to assert if it has been called by mockito_ext.adligo.org.
    * https://www.baeldung.com/java-thread-stop
    */
-  public void toggleInterruptFlag();
+  default void toggleInterruptFlag() {
+    Thread.currentThread().interrupt();
+  }
 }
